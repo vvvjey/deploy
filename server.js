73de2f629920f193d2eef6557a456c1ebaddc991
vvvjey <hoangtu4520031234@gmail.com>
2023-02-26 15:06:02 +0700

    commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer(function(req, res) {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.end('Hello Tus Hoang\n');
 });
 
 server.listen(port, hostname, function() {
